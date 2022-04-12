dcaf328833fcad5d90270b15403d11da1e8d2085
Omar Elbably <ooelbably@gmail.com>
2022-04-12 02:15:45 +0200

    task1

--- OmarElbably/task3/hospital/Number.php
+++ OmarElbably/task3/hospital/Number.php
@@ -1,22 +1,13 @@
 <?php
+session_start();
 
-if ($_POST) {
 
-    $inputNumber = $_POST["inputNumber"];
-    $root = $_POST["Root"];
-    $result = $inputNumber ** (1 / $root);
-
-    $number =
-        "<div class='alert alert-success'>
-        <strong> The Root of </strong> $inputNumber is $result 
-            </div>";
-}
 ?>
 <!doctype html>
 <html lang="en">
 
 <head>
-    <title>Root</title>
+    <title>Hospital</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -29,26 +20,24 @@ if ($_POST) {
     <div class="container">
         <div class="row mt-5">
             <div class="col-12 text-center text-dark">
-                <h4>Root number</h4>
+                <h1 style="color: blue">Hospital</h1>
             </div>
-            <div class="col-4 offset-4 mt-5">
-                <form method="post">
+            <div class="col-6 offset-3 mt-5">
+                <form action="Review.php" method="post">
                     <div class="form-group">
-                        <label for="inputNumber">Enter a Number</label>
-                        <input type="number" name="inputNumber" class="form-control" required>
+                        <label for="phoneNumber">Enter your phone number</label>
+                        <input type="number" name="phoneNumber" class="form-control" required>
                     </div>
-                    <div class="form-group">
-                        <label for="Root">Root</label>
-                        <input type="number" name="Root" class="form-control">
-                    </div>
-                    <button class="btn btn-outline-dark rounded btn-sm mb-5"> Calculate </button>
+
+                    <button class="btn btn-outline-dark rounded btn-sm mb-5"> Submit </button>
                 </form>
                 <?php
-                echo $number ?? "";
+                echo $totalLoan ?? "";
                 ?>
             </div>
         </div>
     </div>
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
